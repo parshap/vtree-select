171dfb7ba4edef6f29e2e842bf3f3328012fdf06
bouzuya <m@bouzuya.net>
2016-03-08 06:47:53 +0900

    Fix invalid test

--- test.js
+++ test.js
@@ -45,7 +45,7 @@ assert.strictEqual(select("*:root#tree").matches(tree), true);
 assert.strictEqual(select("span.span1").matches(tree.children[0]), true);
 assert.strictEqual(select("zz").matches(tree), false);
 assert.strictEqual(select(":not(span)").matches(tree), true);
-assert.strictEqual(select(":not(div)").matches(tree), false);
+assert.strictEqual(select(":not(div)").matches(tree), true);
 
 // Widget
 // Use widget from virtual-dom example
